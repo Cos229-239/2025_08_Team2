094c2a3635a37a0b9d8ef1e6f60ef48de7112cae
mel dommer <mel.dommer@gmail.com>
2025-08-21 19:35:58 -0500

    Adding Patch Raven Background Color!

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Theme.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Theme.kt
@@ -11,19 +11,22 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
+/**
+ * This is the main "Patch Raven Color Scheme"
+ * no dark/light modes.
+ */
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    tertiary = Pink40,
+    background = Background800,
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
@@ -37,7 +40,7 @@ private val LightColorScheme = lightColorScheme(
 fun RavenGamingNewsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,8 +48,7 @@ fun RavenGamingNewsTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
