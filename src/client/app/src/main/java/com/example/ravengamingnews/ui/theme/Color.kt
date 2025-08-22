de458d397226c1072c1edf0b783f025b4ef120bc
mel dommer <mel.dommer@gmail.com>
2025-08-21 19:35:58 -0500

    Adding Patch Raven Background Color!

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Color.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.ravengamingnews.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Template Project Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Patch Raven Official Colors
+val Background800 = Color(0xff4d1b6b)
