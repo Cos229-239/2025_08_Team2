16dd891f849956d1f0b5b97afc24b09b9e8adaaa
mel dommer <mel.dommer@gmail.com>
2025-08-21 20:49:51 -0500

    Changing to use dark.

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Theme.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/theme/Theme.kt
@@ -11,29 +11,21 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-//private val DarkColorScheme = darkColorScheme(
-//    primary = Purple80,
-//    secondary = PurpleGrey80,
-//    tertiary = Pink80
+//private val LightColorScheme = lightColorScheme(
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
 //)
 
 /**
  * This is the main "Patch Raven Color Scheme"
  * no dark/light modes.
  */
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80,
     background = Background800,
-    /* Other default colors to override
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -48,8 +40,8 @@ fun RavenGamingNewsTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-//        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+//        !darkTheme -> lightColorScheme
+        else -> DarkColorScheme
     }
 
     MaterialTheme(
