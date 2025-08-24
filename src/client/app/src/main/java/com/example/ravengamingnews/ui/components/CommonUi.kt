bdfc883246b8a9c13c913bc959643b4f81f37b78
Wesley Filion <WTFilion@student.fullsail.edu>
2025-08-24 14:47:34 -0400

    Adding logo image to CommonUI for visualization testing.

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/components/CommonUi.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/components/CommonUi.kt
@@ -1,5 +1,7 @@
 package com.example.ravengamingnews.ui.components
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -24,6 +27,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -147,6 +151,16 @@ fun TextOnlyButtonPR(
         )
     }
 }
+@Composable
+fun LogoImagePR(modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.patch_raven_logo_ver_3_orange)
+
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+    )
+}
 
 @Composable
 fun TopAppBarButtonPR(
@@ -190,6 +204,11 @@ fun OutlinedTextFieldPreview() {
                 verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center,
                 horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
             ) {
+                LogoImagePR(
+                    modifier = Modifier
+                        .size(250.dp)
+                        .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                )
                 OutlinedTextFieldPR(
                     label = stringResource(R.string.email),
                     value = "test@@test.com",
