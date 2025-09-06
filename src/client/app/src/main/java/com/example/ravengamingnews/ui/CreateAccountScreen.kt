d1ff89c70f67dea46445342c88f2a304c29f60d2
mel dommer <mel.dommer@gmail.com>
2025-09-05 19:03:11 -0500

    Adding No Account Screen

---
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/CreateAccountScreen.kt
@@ -0,0 +1,45 @@
+package com.example.ravengamingnews.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ravengamingnews.R
+import com.example.ravengamingnews.ui.components.ButtonPR
+import com.example.ravengamingnews.ui.components.TextOnlyButtonPR
+import com.example.ravengamingnews.ui.theme.RavenGamingNewsTheme
+
+@Composable
+fun CreateAccountScreen(
+    modifier: Modifier = Modifier,
+    onAccountCreated: () -> Unit = {},
+    onContinueAsGuest: () -> Unit = {},
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.sign_up),
+        )
+        ButtonPR(
+            text = stringResource(R.string.sign_up),
+            onClick = onAccountCreated, // TODO: Needs to actually route to the create account screen
+            modifier = modifier
+        )
+        TextOnlyButtonPR(
+            text = stringResource(R.string.continue_without_account),
+            onClick = onContinueAsGuest,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CreateAccountScreenPreview() {
+    RavenGamingNewsTheme {
+        NoAccountScreen()
+    }
+}
