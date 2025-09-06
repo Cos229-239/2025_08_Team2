eeea2d7ad2f285417de3b9170016e5d9a8d5e9af
Robert Tobiasz <RRTobiasz@student.fullsail.edu>
2025-09-05 22:02:06 -0400

    Login Screen
    Developed and Functional

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/LoginScreen.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/LoginScreen.kt
@@ -1,13 +1,40 @@
 package com.example.ravengamingnews.ui
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ravengamingnews.R
 import com.example.ravengamingnews.ui.components.ButtonPR
+import com.example.ravengamingnews.ui.components.LogoImagePR
 import com.example.ravengamingnews.ui.components.TextOnlyButtonPR
 import com.example.ravengamingnews.ui.theme.RavenGamingNewsTheme
 
@@ -17,24 +44,96 @@ fun LoginScreen(
     onLoginSuccess: () -> Unit = {},
     onCreateAccountClick : () -> Unit = {},
 ) {
-    Column(
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current.applicationContext
+    val focusManager = LocalFocusManager.current
+
+    Box(
         modifier = modifier
-    ) {
-        Text(
-            text = stringResource(R.string.login),
-        )
-        ButtonPR(
-            text = stringResource(R.string.login),
-            onClick = onLoginSuccess, // Replace with real login logic
+            .fillMaxSize()
+            .clickable(
+                indication = null,
+                interactionSource = remember {MutableInteractionSource()}
+            ){
+                focusManager.clearFocus()
+            },
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+
             modifier = modifier
-        )
-        TextOnlyButtonPR(
-            text = stringResource(R.string.dont_have_account_sign_up),
-            onClick = onCreateAccountClick,
-            modifier = modifier
-        )
+                .fillMaxSize()
+                .padding(horizontal = 26.dp, vertical = 140.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LogoImagePR(
+                modifier = Modifier
+                    .size(250.dp)
+                    .padding(0.dp, 0.dp, 0.dp, 0.dp)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                    focusedLabelColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLabelColor = MaterialTheme.colorScheme.primary,
+                    focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                ),
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Password") },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                    focusedLabelColor = MaterialTheme.colorScheme.primary,
+                    unfocusedLabelColor = MaterialTheme.colorScheme.primary,
+                    focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                ),
+
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+            )
+
+            ButtonPR(
+                modifier = Modifier.padding(top = 16.dp),
+                text = stringResource(R.string.login),
+                onClick = {
+                    if (email == "admin" && password == "admin123") {
+                        Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                        onLoginSuccess() // triggers authViewModel.login()
+                    } else {
+                        Toast.makeText(context, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            )
+
+            TextOnlyButtonPR(
+                text = stringResource(R.string.dont_have_account_sign_up),
+                onClick = onCreateAccountClick,
+                modifier = modifier
+            )
+        }
     }
-}
+    }
+
+
 
 @Preview
 @Composable
