30dc89341e4183c1854f04de699616a66638d9d0
mel dommer <mel.dommer@gmail.com>
2025-09-17 22:02:07 -0500

    Relocating one file to remove similar package names.
    I made a util and utils package, dumb oversight by me.  Consolidated into one.

--- src/client/app/src/main/java/com/example/ravengamingnews/ui/ArticlePage.kt
+++ src/client/app/src/main/java/com/example/ravengamingnews/ui/ArticlePage.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.ravengamingnews.utils.toFormattedString
+import com.example.ravengamingnews.util.toFormattedString
 
 @Composable
 fun ArticlePage(
